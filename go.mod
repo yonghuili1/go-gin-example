02bdb491208afd830ab6f54dcd711b5a534cbc84
yonghuili1 <1437891764@qq.com>
2021-04-11 12:05:19 +0800

    test

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/go-ini/ini v1.62.0 // indirect
+	github.com/go-ini/ini v1.62.0
 	github.com/go-playground/validator/v10 v10.5.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
--- pkg/util/pagination.go
+++ pkg/util/pagination.go
@@ -4,15 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/unknwon/com"
 
-	"github.com/EDDYCJY/go-gin-example/pkg/setting"
+	"github.com/yonghuili1/go-gin-example/pkg/setting"
 )
 
 func GetPage(c *gin.Context) int {
 	result := 0
 	page, _ := com.StrTo(c.Query("page")).Int()
-    if page > 0 {
-        result = (page - 1) * setting.PageSize
-    }
+	if page > 0 {
+		result = (page - 1) * setting.PageSize
+	}
 
-    return result
+	return result
 }
