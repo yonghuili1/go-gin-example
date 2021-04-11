64c71263cede6e296c0c1ed6070e55874bdea66a
yonghuili1 <1437891764@qq.com>
2021-04-11 15:47:33 +0800

    add api v1

---
+++ routers/api/v1/tag.go
@@ -0,0 +1,28 @@
+/*
+获取标签列表：GET("/tags”)
+新建标签：POST("/tags”)
+更新指定标签：PUT("/tags/:id”)
+删除指定标签：DELETE("/tags/:id”)
+*/
+
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+//获取多个文章标签
+func GetTags(c *gin.Context) {
+}
+
+//新增文章标签
+func AddTag(c *gin.Context) {
+}
+
+//修改文章标签
+func EditTag(c *gin.Context) {
+}
+
+//删除文章标签
+func DeleteTag(c *gin.Context) {
+}
