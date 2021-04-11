af89e9eb0801677433cc4eca496839a6490fa495
yonghuili1 <1437891764@qq.com>
2021-04-11 13:14:20 +0800

    remove routers from mainj.go

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
-
-    "github.com/yonghuili1/go-gin-example/pkg/setting"
+	"github.com/yonghuili1/go-gin-example/pkg/setting"
+	"github.com/yonghuili1/go-gin-example/routers"
 )
 
 func main() {
-    router := gin.Default()
-    router.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	router := routers.InitRouter()
 
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
